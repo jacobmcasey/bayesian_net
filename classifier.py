bdf9b54aa29b2ca191bb5e2168bc27b28295d1a5
jacobmcasey <71528526+jacobmcasey@users.noreply.github.com>
2023-02-21 14:43:24 +0000

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -8,6 +8,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import to_categorical
 import numpy as np
 
 class Classifier:
@@ -19,20 +20,23 @@ class Classifier:
 
     def fit(self, good_moves_data, target):
 
+        # Convert to one-hot encoded features
+        target_encoded = to_categorical(target, num_classes=4)
+
         # Define the network architecture
         self.model = tf.keras.models.Sequential()
         self.model.add(tf.keras.Input(shape=(25,)))
         self.model.add(tf.keras.layers.Dense(32, activation='relu'))
         self.model.add(tf.keras.layers.Dense(32))
-        self.model.add(tf.keras.layers.Dense(1, activation='softmax'))
-
+        self.model.add(tf.keras.layers.Dense(4, activation='softmax'))
         self.model.summary()
+        
         # Compile the model
-        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
         # Train the model
-        self.model.fit(np.array(good_moves_data), np.array(target), epochs=10, batch_size=None)
+        self.model.fit(np.array(good_moves_data), target_encoded, epochs=10, batch_size=None)
 
     def predict(self, state_data, legal):
         move = self.model.predict(np.array(state_data).reshape(1, 25))
-        return move
+        return np.argmax(move)
